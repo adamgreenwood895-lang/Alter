e4ce0779968ccb9d4d82c6d3d13cdca4d0daa530
adamgreenwood895-lang <adam.greenwood895@gmail.com>
2026-04-07 00:06:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const tapZone = document.getElementById("tapZone");
-const screen1 = document.getElementById("screen1");
 const screen2 = document.getElementById("screen2");
 const bgImage = document.getElementById("bgImage");
 
-// AUTO DAY / NIGHT SWITCH
+// DAY / NIGHT
 const hour = new Date().getHours();
 
 if (hour >= 7 && hour < 19) {
@@ -12,25 +11,11 @@ if (hour >= 7 && hour < 19) {
   bgImage.src = "assets/night.png";
 }
 
-// TAP ACTION
+// TAP
 tapZone.addEventListener("click", () => {
-
-  // subtle feedback
-  document.body.style.opacity = "0.95";
+  document.querySelector(".ui").style.opacity = "0";
 
   setTimeout(() => {
-    document.body.style.opacity = "1";
-  }, 150);
-
-  // transition
-  setTimeout(() => {
-    screen1.classList.add("fade-out");
-
-    setTimeout(() => {
-    screen1.classList.add("hidden");
-screen2.classList.remove("hidden");
-    }, 600);
-
+    screen2.classList.remove("hidden");
   }, 400);
-
 });
