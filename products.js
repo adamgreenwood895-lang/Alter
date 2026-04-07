b5e3c8e6d1651e1cc42211b788e14b6da0592405
adamgreenwood895-lang <adam.greenwood895@gmail.com>
2026-04-07 05:05:16 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,38 @@
+const PRODUCTS = {
+  hoodies: [
+    {
+      name: "Black Essential Hoodie",
+      price: "£60",
+      image: "assets/hoodie1.png"
+    },
+    {
+      name: "Oversized Grey Hoodie",
+      price: "£70",
+      image: "assets/hoodie2.png"
+    }
+  ],
 
+  tshirts: [
+    {
+      name: "Core White Tee",
+      price: "£25",
+      image: "assets/tshirt1.png"
+    }
+  ],
+
+  tracksuits: [
+    {
+      name: "Midnight Tracksuit",
+      price: "£120",
+      image: "assets/tracksuit1.png"
+    }
+  ],
+
+  bags: [
+    {
+      name: "Minimal Crossbody",
+      price: "£45",
+      image: "assets/bag1.png"
+    }
+  ]
+};
