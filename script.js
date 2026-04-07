f9245add8b2d4c0657aebdc0ec5a7c0a9f77176e
adamgreenwood895-lang <adam.greenwood895@gmail.com>
2026-04-07 02:54:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function activateAlter() {
-  document.body.style.transition = "opacity 0.4s ease";
-  document.body.style.opacity = "0";
 
-  setTimeout(() => {
-    window.location.href = "screen2.html";
-  }, 400);
-}
